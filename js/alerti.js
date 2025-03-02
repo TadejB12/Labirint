5f0087bb4a708f1a688c4f332876fcc7dac6e971
TadejB12 <tadej.bensa@gmail.com>
2025-03-02 22:39:07 +0100

    Add files via upload

---
+++ js/alerti.js
@@ -0,0 +1,26 @@
+function navodila(){
+    swal({
+        title: 'Navodila:',
+        text: 'Za premikanje uporabi puščice na tipkovnici. Cilj igre je priti na vrh labirinta.',
+        icon: 'info',
+        button: {
+            text: 'OK',
+            className: 'swal-button-n',
+        }
+    });
+}
+
+function vizitka(){
+    swal({
+        title: 'Avtor:',
+        text: 'Tadej Bensa 4. RB',
+        icon: 'info',
+        button: {
+            text: 'OK',
+            className: 'swal-button-n',
+        }
+    });
+}
+
+
+
